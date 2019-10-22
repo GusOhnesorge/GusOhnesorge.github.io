8156c54e00b1f03b488eeec7b0c2adb8cb00da53
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2019-10-22 15:54:43 -0500

    moving script to login

--- mashup.js
+++ mashup.js
@@ -1,6 +1,1 @@
 var client_id = "efaa5403e2fb4a4aab9cb0fd9cf6d56a";
-
-function signIn(){
-  var location = "https://www.google.com";
-  window.location.href = "https://accounts.spotify.com/authorize?client_id=efaa5403e2fb4a4aab9cb0fd9cf6d56a&redirect_uri=https:%2F%2Fgusohnesorge.github.io%2Fmashup.html&scope=user-read-private%20user-read-email%20playlist-read-private&response_type=token";
-}
