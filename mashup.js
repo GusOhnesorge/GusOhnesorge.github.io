0b83b573132fb373a5033eab73eee0237ed0e1f1
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-22 00:25:56 -0500

    Update mashup.js
    trying out fixing fetch

--- mashup.js
+++ mashup.js
@@ -5,4 +5,13 @@ async function signIn(){
     method: 'GET'
   }
   let resp = fetch("https://accounts.spotify.com/authorize?client_id=efaa5403e2fb4a4aab9cb0fd9cf6d56a&redirect_uri=https:%2F%2Fgusohnesorge.github.io%2Fmashup.html&scope=user-read-private%20user-read-email%20playlist-read-private&response_type=token", opts);
+  fetch(resp)
+    .then(function(data){
+      console.log("fetched the data!")
+      console.log(data.url);
+      window.location.href=data.url;
+    })
+    .catch(function(error){
+      console.log(error);
+    })
 }
