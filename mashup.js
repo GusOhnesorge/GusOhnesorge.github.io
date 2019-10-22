78348145f6dc703c63691ff5a9de2e2eeff37485
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-22 00:05:31 -0500

    Update mashup.js
    fising uri in fetch

--- mashup.js
+++ mashup.js
@@ -4,7 +4,5 @@ async function signIn(){
   let opts = {
     method: 'GET'
   }
-  window.alert("good here");
-  let resp = fetch("https://accounts.spotify.com/authorize?client_id=efaa5403e2fb4a4aab9cb0fd9cf6d56a&redirect_uri=https://gusohnesorge.github.io/mashup.html&scope=playlist-read-private&response_type=token", opts);
-  window.alert("uhhhh");
+  let resp = fetch("https://accounts.spotify.com/authorize?client_id=efaa5403e2fb4a4aab9cb0fd9cf6d56a&redirect_uri=https:%2F%2Fgusohnesorge.github.io%2Fmashup.html&scope=playlist-read-private&response_type=token", opts);
 }
