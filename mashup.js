c9dfca25196203560bbce4cdf7f6566384c47eae
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-21 23:59:22 -0500

    Update mashup.js

--- mashup.js
+++ mashup.js
@@ -4,6 +4,7 @@ async function signIn(){
   let opts = {
     method: 'GET'
   }
+  window.alert("good here");
   let resp = fetch("https://accounts.spotify.com/authorize?client_id=efaa5403e2fb4a4aab9cb0fd9cf6d56a&redirect_uri=https://gusohnesorge.github.io/mashup.html&scope=playlist-read-private&response_type=token", opts);
   window.alert("uhhhh");
 }
