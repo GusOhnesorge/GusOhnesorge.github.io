19ce1af61607e94d86f9b9625294ac6a11c1d771
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2019-10-22 19:00:10 -0500

    Grabbing info update
    actually grabbing info from spotify and using window.alert to read it off

--- mashup.js
+++ mashup.js
@@ -1,16 +1,28 @@
 var client_id = "efaa5403e2fb4a4aab9cb0fd9cf6d56a";
-window.onload = grabinfo;
+const access_tok;
+window.onload = gethash;
 
-async function grabinfo(){
+async function gethash(){
   if(!window.location.hash){ //returns an empty string if there is no hash
     //This happens when a user says no to Spotify
   }
   else{
     //This happens when a user says yes to Spotify
-    var hash = window.location.hash.substring(1).split('&');//This grabs the hash, gets ride of the #, and returns an array split by &
-    var access_splt = hash[0].split("=");
-    var access_tok = access_splt[1];
+    var hash = window.location.hash.substring(1).split('&');//This grabs the hash, gets rid of the #, and returns an array split by &
+    var access_splt = hash[0].split("=");//access_splt is now an array containg the "token" label and then the token itself
+    access_tok = access_splt[1]; //access_tok is used in calls to the Spotify API
     window.location.hash = "";
-    window.alert(access_tok);
   }
+  infoopts{
+    method: 'GET',
+    headers: {
+      'Authorization': "Bearer "+access_tok
+    }
+  }
+  let jsoninfo = await fetch("https://api.spotify.com/v1/me",infoopts);
+  let info = await jsoninfo.json();
+  for(var element of info){
+    window.alert(element);
+  }
+
 }
