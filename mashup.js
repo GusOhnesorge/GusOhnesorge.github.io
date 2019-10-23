ed9d47f969ef234182d7c20d1bc0fb3e153a6531
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-22 22:50:41 -0500

    Update mashup.js

--- mashup.js
+++ mashup.js
@@ -23,7 +23,7 @@ async function gethash(){
   let jsoninfo = await fetch("https://api.spotify.com/v1/me",infoopts);
   let info = await jsoninfo.json();
   for(var element of info){
-    window.alert(element);
+    window.alert("hi");
   }
 
 }
