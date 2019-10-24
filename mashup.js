3e7e64ac9e18ae940362828192a07939236980bc
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2019-10-24 13:38:53 -0500

    Update mashup.js

--- mashup.js
+++ mashup.js
@@ -57,9 +57,9 @@ async function loadplaylists(){
   };
   //let jsoninfo = await fetch("https://api.spotify.com/v1/users/{user_id}/playlists?limit=${n_playlists}",infoopts);
   //let info = await jsoninfo.json();
-  var t = document.querySelector("#playlist_table");
-  var tr, td, contents;
-  window.alert("queryselector problem");
+  //var t = document.querySelector("#playlist_table");
+  //var tr, td, contents;
+  window.alert("variable problem");
   //for(let i = 0; i<info.length; i++){
     //td = document.createElement("td");
     //trow = document.createElement("tr");
