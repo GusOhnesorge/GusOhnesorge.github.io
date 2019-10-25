02ccf9dac228f3093101ab95b090c326e5f84729
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 18:01:54 -0500

    messed up

--- mashup_project/mashuplogin.js
+++ mashup_project/mashuplogin.js
@@ -1,9 +1,9 @@
-var hash;
-var updateinterval;
+var spotify_access_tok;
+var s_updateinterval;
 var popup;
-var url;
+var s_url;
 
-function signIn(){
+function spotifysignIn(){
   var client_id = "efaa5403e2fb4a4aab9cb0fd9cf6d56a";
   var scopes = "user-modify-playback-state%20user-read-private%20user-read-email%20playlist-read-private";
   var redirect_uri = "https:%2f%2fgusohnesorge.github.io%2fmashup_project%2fmashuplogin.html";
@@ -14,22 +14,27 @@ function signIn(){
   var top = (screen.height / 2) - (height / 2);
   url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`;
   popup = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
-  updateinterval = window.setInterval(closepopup, 200);
+  s_updateinterval = window.setInterval(spotifypopup, 200);
 }
 
-function closepopup(){
-  if(popup.location.href != url && !popup.location.hash){ //returns an empty string if there is no hash
-    //This happens when a user says no to Spotify
-    clearInterval(updateinterval);
+function spotifypopup(){
+  if(popup != null){
+    if(popup.location.href != url && !popup.location.hash){ //returns an empty string if there is no hash
+      //This happens when a user says no to Spotify
+      clearInterval(s_updateinterval);
+    }
+    else if(popup.location.hash){
+      //This happens when a user says yes to Spotify
+      var hash = popup.location.hash.substring(1).split('&');//This grabs the hash, gets rid of the #, and returns an array split by &
+      popup.close();
+      var access_split = hash[0].split("=");//access_splt is now an array containg the "token" label and then the token itself
+      spotify_access_tok = access_split[1]; //spotify_access_tok is used in calls to the Spotify API
+      var contents = document.createTextNode(spotify_access_tok);
+      var thediv = document.querySelector("#test");
+      thediv.appendChild(contents);
+    }
   }
-  else if(popup.location.hash){
-    //This happens when a user says yes to Spotify
-    hash = popup.location.hash.substring(1).split('&');//This grabs the hash, gets rid of the #, and returns an array split by &
-    popup.close();
-    var access_split = hash[0].split("=");//access_splt is now an array containg the "token" label and then the token itself
-    spotify_access_tok = access_split[1]; //spotify_access_tok is used in calls to the Spotify API
-    var contents = document.createTextNode(spotify_access_tok);
-    var thediv = document.querySelector("#test");
-    thediv.appendChild(contents);
+  else{
+    clearInterval(s_updateinterval);
   }
 }
