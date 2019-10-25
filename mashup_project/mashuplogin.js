f613af1c9b796cad5b5fa594ba1858ba4cb4a3d7
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 17:09:04 -0500

    Update mashuplogin.js

--- mashup_project/mashuplogin.js
+++ mashup_project/mashuplogin.js
@@ -6,10 +6,10 @@ function signIn(){
   var width = 450;
   var height = 730;
   var left = (screen.width / 2) - (width / 2);
-  //var top = (screen.height / 2) - (height / 2);
-//  var contents = document.createTextNode("test");
-  //var thediv = querySelector("#test");
-  //thediv.appendChild(contents);
+  var top = (screen.height / 2) - (height / 2);
+  var contents = document.createTextNode("test");
+  var thediv = querySelector("#test");
+  thediv.appendChild(contents);
   window.alert("Just kill me");
   //var url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`;
   //var w = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
