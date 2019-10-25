38618dbc59e2630678d20c1a5988fcd5ba012abb
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 17:40:51 -0500

    Update mashuplogin.js

--- mashup_project/mashuplogin.js
+++ mashup_project/mashuplogin.js
@@ -11,9 +11,6 @@ function signIn(){
   var height = 730;
   var left = (screen.width / 2) - (width / 2);
   var top = (screen.height / 2) - (height / 2);
-  var contents = document.createTextNode("test");
-  var thediv = document.querySelector("#test");
-  thediv.appendChild(contents);
   var url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`;
   popup = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
   updateinterval = window.setInterval(closepopup, 200);
@@ -29,6 +26,8 @@ function closepopup(){
     popup.close();
     var access_splt = hash[0].split("=");//access_splt is now an array containg the "token" label and then the token itself
     spotify_access_tok = access_splt[1]; //spotify_access_tok is used in calls to the Spotify API
-    window.alert(hash);
+    var contents = document.createTextNode("test");
+    var thediv = document.querySelector("#test");
+    thediv.appendChild(hash);
   }
 }
