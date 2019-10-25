1003719ddd719bac7dba0771f181263ea52ad1ba
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-24 23:56:15 -0500

    Update mashup.js

--- mashup.js
+++ mashup.js
@@ -138,9 +138,7 @@ async function loadplaylists(){
     let jsoninfo = await fetch(`https://api.spotify.com/v1/me/playlists/${playlist_id}`,infoopts);
     let info = await jsoninfo.json();
     setdevice();
-    let jsoninfo = await fetch(`https://api.spotify.com/v1/me/player`,infoopts);
-    let info = await jsoninfo.json();
-    window.alert(info.device.name);
+    
 
   }
 
