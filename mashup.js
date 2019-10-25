3065634a43df7383826ee62ae812c700c0496cf7
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 00:05:45 -0500

    Update mashup.js

--- mashup.js
+++ mashup.js
@@ -148,7 +148,9 @@ async function loadplaylists(){
     };
     let jsoninfo2 = await fetch(`https://api.spotify.com/v1/me/player`,playeropts);
     let info2 = await jsoninfo2.json();
+    window.alert("wtf");
     window.alert(info2.device.name);
+    window.alert(info2.error);
 
   }
 
