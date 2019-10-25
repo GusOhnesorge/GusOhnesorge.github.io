0126a6d70d10d8c18a60e898ac3d1c8076db71a0
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 17:00:32 -0500

    Update mashuplogin.js

--- mashup_project/mashuplogin.js
+++ mashup_project/mashuplogin.js
@@ -11,6 +11,12 @@ function signIn(){
   var thediv = querySelector("#test");
   thediv.appendChild(contents);
   var url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`;
+  window.addEventListener("message", function(event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                callback(hash.access_token);
+            }
+        }, false);
   var w = window.open(url,
                      'Spotify',
                      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
