beb7b1c560898b9600201bee067c3d4528e6dd69
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 16:42:28 -0500

    making new js file

---
+++ mashuplogin.js
@@ -0,0 +1,17 @@
+function signIn(){
+  var client_id = "efaa5403e2fb4a4aab9cb0fd9cf6d56a";
+  var scopes = "user-modify-playback-state%20user-read-private%20user-read-email%20playlist-read-private";
+  var redirect_uri = "https:%2f%2fgusohnesorge.github.io%2fmashuplogin.html";
+  var response_type = "token";
+  var width = 450,
+      height = 730,
+      left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+  var contents = document.createTextNode("test");
+  var thediv = querySelector("#test");
+  thediv.appendChild(contents);
+  var w = window.open(`https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`,
+                     'Spotify',
+                     'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                    );
+}
