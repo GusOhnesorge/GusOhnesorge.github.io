552dd7d7386d093df8ba07cf85770c7d9ee9d250
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 22:28:07 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -74,8 +74,8 @@ async function geniuspopup(){
       //getting authorization_code from genius
       window.alert("blerg");
       let the_body = {
-        "client_id" : genius_client_id,
-        "code" : genius_access_tok,
+        "client_id" : "GtAcwB5MChoR-I0AVk71blFtVm-7G-MnNv3WOur_4T4sKZ-4FVlEDWzr7ShTzTny",
+        "code" : 'JESUS CHRIST',
         "client_secret" : "C_3rJhRuvSV7Z4dUSmB4pJa1fJNKwMOD8sYWVyUf3jzwqGo19zLLaCtcroWxlXZTtvepIVGhugZUBVChSuendw", //should not technically hardcode in client secret
         "redirect_uri" : "https://gusohnesorge.github.io/mashup_project/mashup.html",
         "response_type" : "code",
