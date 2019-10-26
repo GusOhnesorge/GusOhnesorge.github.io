2a8304d56bdcd9f631d9c0a3bd16843d5e48fdbb
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 20:49:26 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -112,6 +112,7 @@ async function geniuspopup(){
     window.alert("2");
     let info = await jsoninfo.json();
     window.alert("3");
+    window.alert(JSON.stringify(info));
     var contents = document.createTextNode(JSON.stringify(info));
     var thediv = document.querySelector("#lyrics");
     thediv.appendChild(contents);
