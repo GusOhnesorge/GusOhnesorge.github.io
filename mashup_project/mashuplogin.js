b8adf74e70926f23771dc8f0325f4e1567eacacf
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-26 00:20:13 -0500

    revert

--- mashup_project/mashuplogin.js
+++ mashup_project/mashuplogin.js
@@ -1,6 +1,6 @@
 function signIn(){
   var client_id = "efaa5403e2fb4a4aab9cb0fd9cf6d56a";
-  var scopes = "user-modify-playback-state%20user-read-private%20user-read-email%20playlist-read-private";
+  var scopes = "user-modify-playback-state%20user-read-private%20user-read-email%20playlist-read-private%20user-read-currently-playing";
   var redirect_uri = "https:%2f%2fgusohnesorge.github.io%2fmashup_project%2fmashup.html";
   var response_type = "token";
   window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${response_type}`;
