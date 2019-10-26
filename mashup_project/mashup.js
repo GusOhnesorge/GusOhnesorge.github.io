cc336f2c779dcd169af3d4483ab067f3ab9ad41a
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 19:47:15 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -86,8 +86,7 @@ async function geniuspopup(){
       window.alert("2");
       let info = await jsoninfo.json();
       window.alert("3");
-      //genius_access_tok = info.response.access_token;
-      window.alert(info.meta.message);
+      genius_access_tok = jsoninfo.access_token;
       window.alert(genius_access_tok);
       var contents = document.createTextNode(genius_access_tok);
       var thediv = document.querySelector("#lyrics");
