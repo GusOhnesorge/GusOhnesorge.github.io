143e9dbe54fbef71ae8936059169d82d266310bf
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-26 13:59:38 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -49,6 +49,7 @@ async function wikirequest(title){
   fetch(url, wikiopts)
   .then(function(response){return response.json})
     .then(function(data){
+      window.alert(data);
       for(page in data.query.pages){
         window.alert(page.title);
         window.alert(page.extract);
