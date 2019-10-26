8dcc3f7f4b8ea915d3ac17798884c1f5faa98267
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-25 20:29:57 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -73,19 +73,19 @@ async function geniuspopup(){
       let infoopts = {
         method: 'POST',
         body: JSON.stringify({
-        "client_id": genius_client_id,
-        "code": genius_code,
-        "client_secret": "C_3rJhRuvSV7Z4dUSmB4pJa1fJNKwMOD8sYWVyUf3jzwqGo19zLLaCtcroWxlXZTtvepIVGhugZUBVChSuendw", //should not technically hardcode in client secret
-        "redirect_uri": "https://gusohnesorge.github.io/mashup_project/mashup.html",
-        "response_type": "code",
-        "grant_type": "authorization_code"
+        "client_id" : genius_client_id,
+        "code" : genius_code,
+        "client_secret" : "C_3rJhRuvSV7Z4dUSmB4pJa1fJNKwMOD8sYWVyUf3jzwqGo19zLLaCtcroWxlXZTtvepIVGhugZUBVChSuendw", //should not technically hardcode in client secret
+        "redirect_uri" : "https://gusohnesorge.github.io/mashup_project/mashup.html",
+        "response_type" : "code",
+        "grant_type" : "authorization_code"
         })
       };
-      window.alert("1");
+      window.alert("4");
       let jsoninfo = await fetch("https://api.genius.com/oauth/token",infoopts);
-      window.alert("2");
+      window.alert("5");
       let info = await jsoninfo.json();
-      window.alert("3");
+      window.alert("6");
       genius_access_tok = info.access_token;
       window.alert(JSON.stringify(info));
       var contents = document.createTextNode(genius_access_tok);
