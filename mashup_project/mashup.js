4677f3cdf0872f44b3840e1d72c46de36dbfb2d6
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-26 13:44:33 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -44,7 +44,8 @@ async function wikirequest(title){
       'Content-Type': "application/json",
     }
   };
-  var url = `https://www.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=json&exintro=&titles=${title}`;
+  var url = `https://en.wikipedia.org/api/rest_v1/page/summary/${title}`;
+  //var url = `https://www.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=json&exintro=&titles=${title}`;
   fetch(url, wikiopts)
   .then(function(response){return response.json})
     .then(function(data){window.alert(data)})
