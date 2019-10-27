d7a01f72caa9d4f60bae93287e048a9e44c02d1b
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-27 01:54:29 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -81,7 +81,7 @@ async function wikirequest(title){
 
 });
 }
-function isredirect(text){
+function isredirect(parsed_text){
   if(parsed_text.substring(42, 53) == "redirectMsg"){
     console.log("IS A REDIRECT");
     return true;
