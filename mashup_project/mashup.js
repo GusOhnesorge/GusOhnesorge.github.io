f238073b7e427a11dae37d9aaf50934d86ace7ad
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-26 23:18:29 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -45,7 +45,7 @@ async function wikirequest(title){
   //var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + title + "&callback=?";
   return $.getJSON(url);
 
-};
+}
 //var url = `https://en.wikipedia.org/api/rest_v1/page/summary/${title}`;
   /*var url = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=jsonfm&exintro&redirects=1&titles=meme`;
   fetch(url, wikiopts)
@@ -58,7 +58,6 @@ async function wikirequest(title){
       }
     })
     .catch(function(error){{window.alert(error.message)}})*/
-}
 
 async function loadwiki(){
   var name = document.querySelector("#artist_name");
