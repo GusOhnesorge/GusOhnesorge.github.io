d3bb9ef2e2ee7f8cd690040d60e990a12c7e8e9a
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-27 15:39:37 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -120,7 +120,7 @@ function isredirect(parsed_text){
 }
 
 function isdisamb(parsed_text, title){
-  console.log(parsed_text.substring(42+title.length, 54+title.length));
+  console.log(parsed_text.substring(41+title.length, 53+title.length));
   if(parsed_text.substring(42+title.length, 54+title.length) == "may refer to"){ //This will always be here relative to the title name if the page is a disambiguation page
     return true;
   }
