76425dfb893bca6b4f3bec2f85e187b895456f2a
GusOhnesorge <ohnesorge.gus@gmail.com>
2019-10-26 23:15:52 -0500

    Update mashup.js

--- mashup_project/mashup.js
+++ mashup_project/mashup.js
@@ -40,16 +40,11 @@ async function wikirequest(title){
   /*let wikiopts = {
     mode: "no-cors",
   }*/
-  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=" + title + "&callback=?";
-  $.getJSON(url, function(data) {
-  for(var i = 0; i < 10; i++) {
-    var result = data.query.search[i];
-    console.log(result);
-    $("#wiki_body" + i).html("<h2 id = 'title'" + i + ">" + result.title + "</h2");
-    $("#wiki_body" + i).append("<p id = 'snippet'" + i + ">" + result.snippet + "</p");
-    $("#wiki_body" + i).attr("href", "https://en.wikipedia.org/wiki/" + result.title);
-    $("#wiki_body" + i).css("visibility", "visible");
-  }
+  title = title.replace(" ", "_");
+  var url = `https://en.wikipedia.org/w/api.php?action=parse&prop=text&page=${title}&format=json`;
+  //var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + title + "&callback=?";
+  return $.getJSON(url);
+
 });
 //var url = `https://en.wikipedia.org/api/rest_v1/page/summary/${title}`;
   /*var url = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=extracts&format=jsonfm&exintro&redirects=1&titles=meme`;
@@ -71,8 +66,8 @@ async function loadwiki(){
   //for(var page in results.query.pages){
     var title = document.querySelector("#wiki_title");
     var body = document.querySelector("#wiki_body");
-    title.innerHTML = "test";
-    body.innerHTML = "me";
+    title.innerHTML = results.parse.title;
+    body.innerHTML = results.parse.text;
 //  }
 }
 
