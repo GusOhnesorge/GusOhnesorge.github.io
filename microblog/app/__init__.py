4de2549fb2a197d492287380b009172833a415dd
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2019-12-11 22:09:02 -0600

    Finished up to part 11 of tutorial, skipped part 10

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from config import Config
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -12,6 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 login.login_view = 'login'
 if not app.debug:
     if app.config['MAIL_SERVER']:
