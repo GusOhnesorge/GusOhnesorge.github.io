2e754d755a42fa90dd3a16dec428940d51937d55
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2020-09-01 11:57:13 -0500

    Update index.js
    colors of project divs randomize and then increase in hue to try to create a gradient

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-		var min_color = 195
-		var max_color = 300
+		var min_color = 0
+		var max_color = 360
 		var sat = 93
 		var light = 93
-		var projects = getElementsByClassName("project")
+		var projects = document.getElementsByClassName("project")
 		var num_projects = projects.length
 		var rand_num = -1
 		function getColor (min, max, colors_left){
 			return Math.floor(Math.random() * (max-min-colors_left)) + min
 		}
-		for project in projects {
-			min_color = getColor(min_color, max_color, num_projects)
-			color_string = "hsl("+min_color.toString()+","+sat.toString()+"%,"+light.toString()+"%)"
-			project.style.backgroundColor = color_string
-			num_projects = num_projects - 1
+		for(i = 0; i<num_projects; i++) {
+			min_color = getColor(min_color+1, max_color, num_projects-i)
+			console.log(min_color)
+			color_string = "hsl("+min_color+","+sat+"%,"+light+"%)"
+			console.log(color_string)
+			projects[i].style.backgroundColor = color_string
 		}
 }, false);
